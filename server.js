e1fc29eab1068d4134ee5fd26fac3ea0cc794710
Himanshu Vyas <hvyas7459@gmail.com>
2026-04-11 16:04:10 +0530

    origin fixed

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express()
 
 // MIDDLEWARE
 app.use(cors({
-  origin: "https://prep-me-482b.vercel.app",
+  origin: "https://prep-me-lilac.vercel.app",
   credentials: true
 }))
 app.use(express.json())
